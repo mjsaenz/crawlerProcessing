20da3bd1e698898b983d5ea9de43bd81cb07a361
Spicer Bak <spicer.bak.frf@gmail.com>
2021-11-18 16:34:03 -0500

    updated repo to hold example data, and processing tools

---
+++ examples/GPScorrect.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from pygeodesy import geoids
+
+def correctEllipsoid(fname, geoidFile='g2012bu8.bin', plot=False):
+    """ This function loads the csv file that is output by the greensea software and will correct the GGA string
+    elevations and geiod to the 2012B geoid that is commonly used at the FRF.  It does this by taking the elevation
+    from the GGA string, subtracting the GGA string geoid, leaving you with raw ellipsoid value.  That ellipsoid
+    value is then corrected with the 2012B geoid that is commonly used at the FRF.
+    
+    NOTE: if this is the first time you're using this, you'll likely have to go get the geoid bin file.  Code was
+        developed using the uncompressed bin file.  It is unclear if the pygeodesy library requires the bin file to
+        be uncompressed.  https://geodesy.noaa.gov/GEOID/GEOID12B/GEOID12B_CONUS.shtml
+        
+    Args:
+        fname: input file to be corrected
+        geoidFile: location of your geoidfile (default='g2012bu8.bin')
+        plot: show an xy scatter plot with elevations
+        
+    Returns:
+         a pandas object with original and corrected values
+         
+    """
+    
+    data = pd.read_csv(fname, header=4)
+    data['ellipsoid'] = data.gga_altitude_m - data.gga_height_geoid_m
+    instance = geoids.GeoidG2012B(geoidFile)
+    geoidHeight = instance.height(data.latitude, data.longitude)
+    data['elevation_NAVD88_m'] = data.ellipsoid + geoidHeight
+    if plot is True:
+        data.plot.scatter('longitude', 'latitude', c='elevation_NAVD88_m', cmap='ocean')
+    
+    return data
+
+fname = "data/20211020_155001.380_telemetry.gssbin_GPS_STAT_2.csv"
+data = correctEllipsoid(fname, plot=True)
